53918d55a904682e26bbeba257546f4d33427b6a
DaniilsKezins <144620248+DaniilsKezins@users.noreply.github.com>
2024-03-02 18:05:21 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,1 +1,34 @@
+// 000RDB000 Jānis Programmētājs 1
 
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("000RDB000 Jānis Programmētājs 1. grupa");
+
+		String fileName;
+		System.out.println("input file name:");
+		fileName = sc.nextLine();
+		sc.close();
+
+		FileReader fr = new FileReader(fileName);
+		Scanner fileScanner = new Scanner(fr);
+
+		// ievadīto no tastatūras faila nosaukumu fileName nemodificēt,
+		// nepievienot mapes vārdu, nepievienot tipu txt
+
+		while (fileScanner.hasNextLine()) {
+			System.out.println(fileScanner.nextLine());
+		}
+
+		System.out.println("result:");
+
+		// izvadīt rezultātus
+
+	}
+
+}
